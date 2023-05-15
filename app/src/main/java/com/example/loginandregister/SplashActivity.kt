f0ae1d04b289cbe4dbd62c7eefa2521e0ae77c8c
Muhammad Dila <mochammeddila21@gmail.com>
2023-05-15 16:46:38 +0700

    Login and Register

--- app/src/main/java/com/example/loginandregister/SplashActivity.kt
+++ app/src/main/java/com/example/loginandregister/SplashActivity.kt
@@ -1,14 +1,24 @@
 package com.example.loginandregister
 
 import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.view.WindowManager
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            val w = window
+            w.setFlags(
+                WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+                WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
+            )
+        }
 
         Handler().postDelayed({
             startActivity(Intent(this@SplashActivity, MainActivity::class.java))
